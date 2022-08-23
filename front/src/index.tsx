ce0fbdd1af1c8635f0e3e82e1273773155ec8b1f
chspower1 <chspower1@gmail.com>
2022-08-23 14:32:54 +0900

    restart

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const queryClient = new QueryClient();
 root.render(
     <React.StrictMode>
-        <App />
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </RecoilRoot>
     </React.StrictMode>
 );
