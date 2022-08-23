9985740454dc7ef05ec8705321f5de33b0265cd6
Juni Lee <wnsdml0120@korea.ac.kr>
2022-08-23 10:42:16 +0900

    add comments to UserSchema

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -1,26 +1,33 @@
+// @ts-ignore
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
   {
+    // User 식별 id
     id: {
       type: String,
       required: true,
     },
+    // User email
     email: {
       type: String,
       required: true,
     },
+    // User 이름
     name: {
       type: String,
       required: true,
     },
+    // 비밀번호
     password: {
       type: String,
       required: true,
     },
+    // 프로필 사진
 		picture: {
       type: String, //Image URL
     },
+    // 설명
 		description: {
       type: String,
       required: false,
