8eb87ef28638459cf17e0eecf6ecf33c5310fe44
handongryong <think8867@gmail.com>
2022-08-23 20:33:08 +0900

    8/23 프로젝트 컴포넌트 구현

--- front/src/components/project/Project.tsx
+++ front/src/components/project/Project.tsx
@@ -1,5 +1,51 @@
-import { IProject } from "../../atoms";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { errorSelector, useRecoilState } from "recoil";
+import { IProject, usersState } from "../../atoms";
+import axios from "axios";
+import { type } from "os";
+import { ProjectEditForm } from "./ProjectEditForm";
 
-export default function Project( ifon : IProject[]) {
-    return null;
+
+export default function Project(info: IProject[]) {
+  const [isAdding, setIsAdding] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<IProject>();
+
+  function handleAdding() {
+    setIsAdding((isAdding) => !isAdding);
+  }
+
+    const[projects,setProjects] = useState<IProject[]>([
+        {
+            title:"elice",
+            startDate:new Date(),
+            endDate:new Date(),
+            description:"asd"
+        }
+    ])
+
+  return (
+    <>
+      <h1>프로젝트</h1>
+      <div>
+        <ul>
+            {projects.map((val:IProject,index) =>
+                (
+                    <li key={index}>
+                        <h1>{val.title}</h1>
+                    </li>
+                )
+            )}
+        </ul>
+      </div>
+      <div>
+        {isAdding && <ProjectEditForm setIsAdding={setIsAdding}/>}
+        <button onClick={handleAdding}>추가</button>
+      </div>
+    </>
+  );
 }
---
+++ front/src/components/project/ProjectEditForm.tsx
@@ -0,0 +1,70 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { IProject, usersState } from "../../atoms";
+
+
+export const ProjectEditForm=({setIsAdding}:any)=>{
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting, errors },
+      } = useForm<IProject>();
+
+    return(
+        <form
+            onSubmit={handleSubmit((data) => {
+              alert(JSON.stringify(data));
+              setIsAdding(false)
+            })}
+          >
+            <input
+              type="text"
+              id="project-title"
+              placeholder="프로젝트 명"
+              {...register("title", {
+                required: "프로젝트명을 입력해주세요",
+                shouldUnregister: true,
+              })}
+            ></input>
+            {errors.title?.message && errors.title.message}
+            <input
+              type="number"
+              id="project-startDate"
+              placeholder="프로젝트 시작기간"
+              {...register("startDate", {
+                required: "시작기간을 입력해주세요",
+                pattern: {
+                  value: /^\d{4}\d{2}\d{2}$/,
+                  message: "20220101 형식으로 작성해주세요",
+                },
+                shouldUnregister: true,
+              })}
+            ></input>
+            {errors.startDate?.message && errors.startDate.message}
+            <input
+              type="number"
+              id="project-endDate"
+              placeholder="프로젝트 종료기간"
+              {...register("endDate", {
+                required: "종료기간을 입력해주세요",
+                pattern: {
+                  value: /^\d{4}\d{2}\d{2}$/,
+                  message: "20220101 형식으로 작성해주세요",
+                },
+                shouldUnregister: true,
+              })}
+            ></input>
+            {errors.endDate?.message && errors.endDate.message}
+            <input
+              type="text"
+              id="project-description"
+              placeholder="추가설명"
+              {...register("description", { shouldUnregister: true })}
+            ></input>
+            <button>추가</button>
+            <button onClick={()=>setIsAdding(false)}>취소</button>
+          </form>
+    )
+}
