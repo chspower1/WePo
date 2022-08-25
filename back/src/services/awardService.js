403d55ace886501f7f778f2d8a4b6edf3f7f4da8
Juni Lee <wnsdml0120@korea.ac.kr>
2022-08-25 16:25:30 +0900

    update: All services

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -2,19 +2,20 @@ import { Award } from "../db/models/Award";
 
 class awardService {
 
+  // 수상내역 추가
   static async addAward({ userId, title, grade, org, date, description }) {
-    // 새 수상내역 추가
     // 에러 처리 추가하기
     return Award.create({ userId, title, grade, org, date, description });
   }
 
-  static async getAllAwards({userId}) {
-    // 모든 수상내역 불러오기
+  // userId에 해당하는 유저의 수상내역 전체 조회
+  static async getAllAwards({ userId }) {
     // 에러 처리 추가하기 
-    return Award.findByUserId({userId});
+    return Award.findByUserId({ userId });
   }
 
-  static async updateAward({awardId, toUpdate}) {
+  // 수상내역 수정
+  static async updateAward({ awardId, toUpdate }) {
     const { title, grade, org, date, description } = toUpdate;
     const newValues = {
       ...(title && { title }),
@@ -23,11 +24,12 @@ class awardService {
       ...(date && { date }),
       ...(description && { description }),
     }
-    return Award.update({awardId, newValues});
+    return Award.update({ awardId, newValues });
   }
 
-  static async deleteAward({awardId}){
-    return Award.delete({awardId})
+  // 수상내역 삭제
+  static async deleteAward({ awardId }){
+    return Award.delete({ awardId })
   }
 }
 
--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -1,14 +1,10 @@
 import { Education } from "../db";
-import { v4 as uuidv4 } from "uuid";
 
 class educationService {
 
   // 학력 추가
   static async addEducation({ userId, school, major, status }) {
-
-    // 해당 학력에 대한 유니크 id 부여
-    const eduId = uuidv4();
-    const newEducation = { eduId, userId, school, major, status };
+    const newEducation = { userId, school, major, status };
 
     if(!school || !major || !status) {
       const errorMessage = 
@@ -22,24 +18,15 @@ class educationService {
     return createdNewEducation;
   }
 
-
   // userId에 해당하는 유저의 학력 전체 조회
   static async getEducationListByUserId({ userId }) {
-    const educationList = await Education.findByUserId({ userId });
-    return educationList;
+    return Education.findByUserId({ userId });
   }
-
-  // eduId에 해당하는 학력정보 조회
-  static async getEducationByEduId({ eduId }) {
-    const education = await Education.findOneByEduId({ eduId });
-    return education;
-  }
-
-
+  
   // 학력 수정
   static async updateEducation({ eduId, toUpdate }) {
     // 해당 eduId의 학력정보가 db에 존재하는지 여부 확인
-    let education = await Education.findOneByEduId({ eduId });
+    let education = await Education.findByEduId({ eduId });
 
     // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!education) {
@@ -62,7 +49,7 @@ class educationService {
   // 학력 삭제
   static async deleteEducation({ eduId }) {
     // 해당 eduId의 학력정보가 db에 존재하는지 여부 확인
-    let education = await Education.findOneByEduId({ eduId });
+    let education = await Education.findByEduId({ eduId });
 
     // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!education) {
@@ -71,10 +58,8 @@ class educationService {
       return { errorMessage };
     }
 
-    const deleteSuccessful = await Education.delete({ eduId });
-    return deleteSuccessful;
+    return Education.delete({ eduId });
   }
-  
 }
 
 export { educationService };
