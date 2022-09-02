6ddcc136ae206016578227634abc7d8f9f5a2262
Juni <wnsdml0120@gmail.com>
2022-09-02 11:50:58 +0900

    revert like fix

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -161,13 +161,7 @@ class userAuthService {
       return { errorMessage };
     }
 
-    const detailedLikes = await User.getDetailedLikes(userId)
-    const detailedUser = {
-        ...user,
-        likes: detailedLikes
-    }
-
-    return detailedUser;
+    return user;
   }
 
 
@@ -189,6 +183,7 @@ class userAuthService {
   // 사용자 즐겨찾기 추가/삭제
   static async toggleLike({userId, otherId}) {
     const user = await User.findByUserId(userId);
+    const otherUser = await User.findByUserId(otherId);
     // db에서 찾지 못한 경우, 에러 메시지 반환
     if (!user) {
       const errorMessage =
@@ -197,21 +192,26 @@ class userAuthService {
     }
     
     // 아이디가 존재하면 즐겨찾기에서 삭제
-    if(user.likes.includes(otherId)){
+    if(user.likes.filter(likeUser => likeUser.userId === otherId).length) {
       const newValues = {
-        likes: user.likes.filter( id => id!==otherId )
-      }
-      return User.update({ userId, newValues })
+        likes: user.likes.filter(likeUser => likeUser.userId !== otherId)
+      };
+      return User.update({ userId, newValues });
     }
     
     // 아이디가 없으면 즐겨찾기에 새로 추가
     const newValues = {
-      likes: [ ...user.likes, otherId ]
-    }
-    console.log(newValues)
+      likes: [
+        ...user.likes, {
+        userId: otherId,
+        name: otherUser.name,
+        email: otherUser.email,
+        picture: otherUser.picture
+      }]
+    };
+
     return User.update({ userId, newValues })
   }
-
 
   // 사용자 비밀번호 초기화
   static async resetPassword(email){
